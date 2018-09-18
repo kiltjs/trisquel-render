1615ac574dc5b92ccb21efe63845ce8a12df97e4
Jesús Germade <jesus@germade.es>
2018-09-18 14:22:45 +0200

    playing with TRAVIS browsers

--- karma.conf.js
+++ karma.conf.js
@@ -35,7 +35,8 @@ module.exports = function(config) {
   };
 
   if(process.env.TRAVIS){
-    configuration.browsers = [ 'Chrome_no_sandbox', 'Firefox' ];
+    // configuration.browsers = [ 'Chrome_no_sandbox', 'Firefox' ];
+    configuration.browsers = [ 'Chrome', 'Firefox' ];
     configuration.concurrency = 1;
   }
 
